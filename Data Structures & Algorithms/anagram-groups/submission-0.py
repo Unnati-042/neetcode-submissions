dbbd007c0ec9c73431b5b78e4d4e4d6092c3bce6
Unnati-042 <unnatim200@gmail.com>
2026-06-23 06:38:52 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res={}#mapping  chharacter count  to list  of anagrrams
+        for s in strs :
+            count=[0]*26 #a...........z
+            for c in s :
+                count[ord(c)-ord("a")] +=1
+            res[tuple(count)].append(s)
+
+        return res.values()
+        0(m*n)
+
+
+      
+        
+        
